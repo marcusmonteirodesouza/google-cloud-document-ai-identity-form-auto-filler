59c50d1c4cf755bb79b131eaf4c7e94ab9ee7076
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-08-09 16:06:43 -0400

    Grant GCS service account permissions on the doc ai kms key

--- infra/deployment/terraform/apps/modules/api/kms.tf
+++ infra/deployment/terraform/apps/modules/api/kms.tf
@@ -8,6 +8,9 @@ locals {
   single_region = var.doc_ai_region == "us" ? "us-central1" : "europe-west4" # See https://cloud.google.com/document-ai/docs/cmek#using_cmek
 }
 
+data "google_storage_project_service_account" "gcs_sa" {
+}
+
 resource "google_kms_key_ring" "keyring" {
   name     = "api-${var.region}-key-ring"
   location = var.region
@@ -29,20 +32,9 @@ resource "google_kms_crypto_key_iam_member" "cloud_run_service_agent_api" {
   member        = "serviceAccount:${local.cloud_run_service_agent_email}"
 }
 
-resource "google_kms_key_ring" "doc_ai_keyring" {
-  name     = "doc-ai-${var.doc_ai_region}-keyring"
-  location = var.doc_ai_region
-}
-
 resource "google_kms_key_ring" "doc_ai_keyring_single_region" {
   name     = "doc-ai-${local.single_region}-keyring"
   location = local.single_region
-}
-
-resource "google_kms_crypto_key" "doc_ai" {
-  name            = "doc-ai-key"
-  key_ring        = google_kms_key_ring.doc_ai_keyring.id
-  rotation_period = local.kms_crypto_key_rotation_period
 }
 
 resource "google_kms_crypto_key" "doc_ai_single_region" {
@@ -51,14 +43,14 @@ resource "google_kms_crypto_key" "doc_ai_single_region" {
   rotation_period = local.kms_crypto_key_rotation_period
 }
 
-resource "google_kms_crypto_key_iam_member" "doc_ai_sa_doc_ai" {
-  crypto_key_id = google_kms_crypto_key.doc_ai.id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:${local.doc_ai_sa}"
-}
-
 resource "google_kms_crypto_key_iam_member" "doc_ai_sa_doc_ai_single_region" {
   crypto_key_id = google_kms_crypto_key.doc_ai_single_region.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${local.doc_ai_sa}"
+}
+
+resource "google_kms_crypto_key_iam_member" "gcs_sa_doc_ai_single_region" {
+  crypto_key_id = google_kms_crypto_key.doc_ai_single_region.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:${data.google_storage_project_service_account.gcs_sa.email_address}"
 }
